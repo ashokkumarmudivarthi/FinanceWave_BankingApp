22b4304f57ee62bf109473ab9becdf98b4f9cae8
Ashokkumarmudivarthi <sureshkmpt54@gmail.com>
2026-05-23 19:41:42 +0530

    Transaction service,auth and account services fully working 23/05/2026 7:40PM

---
+++ financewave-beneficiary-service/src/main/java/com/financewave/beneficiary/FinancewaveBeneficiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.financewave.beneficiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancewaveBeneficiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancewaveBeneficiaryServiceApplication.class, args);
+	}
+
+}
---
+++ financewave-beneficiary-service/src/test/java/com/financewave/beneficiary/FinancewaveBeneficiaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financewave.beneficiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancewaveBeneficiaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
